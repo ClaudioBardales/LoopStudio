b30df05add4586437a178eb940773ebcc4f83611
Claudio <claudio@Claudios-Mac.local>
2021-11-07 14:51:13 -0800

    added styled components

--- loop-studio/src/App.js
+++ loop-studio/src/App.js
@@ -1,9 +1,13 @@
-function App() {
+import React from 'react';
+import GlobalStyle from './GlobalStyles';
+
+const App = () => {
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>Hello World</h1>
     </div>
   );
-}
+};
 
 export default App;
---
+++ loop-studio/src/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+ *{
+    box-sizing:border-box;
+    padding: 0;
+    margin: 0;
+ }
+
+
+
+
+
+`;
+
+export default GlobalStyle;
