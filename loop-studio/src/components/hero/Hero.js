579db93ef2d016b9fc911006a42626a44f67bad1
Claudio <claudio@Claudios-Mac.local>
2021-11-20 22:36:15 -0800

    almost done with hero page

--- loop-studio/src/components/hero/Hero.js
+++ loop-studio/src/components/hero/Hero.js
@@ -7,6 +7,15 @@ const Hero = () => {
   return (
     <Img>
       <Navbar />
+      <Wrapper>
+        <Styledh1>
+          <p>
+            IMMERSIVE <br />
+            EXPERIENCES <br />
+            THAT DELIVER
+          </p>
+        </Styledh1>
+      </Wrapper>
     </Img>
   );
 };
@@ -15,7 +24,29 @@ const Img = Styled.div`
 background-image: url(${HeroImg});
 background-repeat: no-repeat;
 background-size: cover;
-min-height: 75vh;
+min-height: 50vh;
+`;
+
+const Wrapper = Styled.div`
+margin: auto;
+width: 71%;
+`;
+
+const Styledh1 = Styled.div`
+display: flex;
+justify-content: flex-start;
+align-items: center;
+border: white 1px solid;
+width: 21.063rem;
+height: 14rem;
+padding: 1.125rem;
+
+p{
+  font-size: 2.625rem;
+  color: white;
+  font-weight: lighter;
+}
+
 `;
 
 export default Hero;
