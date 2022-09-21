44f7db3ef9b253a95c7693d1c8eb64faa7ed2ab6
Claudio <claudio@Claudios-Mac.local>
2022-09-21 16:20:01 -0700

    Finished Mobile Version of Hero Img

--- loop-studio/src/components/hero/Hero.js
+++ loop-studio/src/components/hero/Hero.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Styled from 'styled-components';
 import Navbar from '../Nav/Navbar';
 import HeroImg from '../../img/desktop/image-hero.jpg';
+import MobileHeroImg from '../../img/mobile/image-hero.jpg';
 
 const Hero = () => {
   return (
@@ -25,36 +26,36 @@ background-image: url(${HeroImg});
 background-repeat: no-repeat;
 background-size: cover;
 min-height: 50vh;
+@media (max-width: 500px){
+  background-image: url(${MobileHeroImg});
+  min-height: 80vh;
+}
 `;
 
 const Wrapper = Styled.div`
 margin: auto;
 width: 77%;
-@media (max-width: 768px){
+@media (max-width: 500px){
  display: flex;
  justify-content: center;
+ align-items: center;
+ min-height: 100vh;
+
 `;
 
 const Styledp = Styled.div`
 display: flex;
 justify-content: flex-start;
 align-items: center;
-border: white 1px solid;
+border: white 2px solid;
 width: 21.063rem;
 height: 14rem;
 padding: 1.125rem;
-
 p{
   font-size: 2.625rem;
   color: white;
   font-weight: lighter;
 }
-
-
-}
-
-
-
 `;
 
 export default Hero;
